3074b4e24d172ebffc409d36870277e21097bb0a
Alexmiroha <Alexmiroha@mail.ru>
2022-02-19 02:18:36 +0100

    added some styles and functionality to LoginComponent

--- src/components/reusableComponents/Inputs/Input.jsx
+++ src/components/reusableComponents/Inputs/Input.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import s from './Input.module.css'
 
-const Input = ( {input, meta, ...props} ) => {
+const Input = ({input, meta, ...props}) => {
     // рест оператор, деструктуризація
 
-    console.log({input, meta})
     const checkInput = meta.touched && meta.error;
+    const checkFocused = meta.active || input.value;
+
 
     return (
         <div className={s.input_wrapper}>
-            <label className={s.name}>{props.placeholder}</label>
-            <input className={checkInput ? `${s.errorInput} ${s.input}` : `${s.input}`} type="text" {...meta} {...input} />
-            {checkInput && <span className={s.errorSpan}>{meta.error}</span>}
+            <input className={checkInput ? `${s.errorInput} ${s.input}` : `${s.input}`}
+                   type="text" {...meta} {...input} />
+            <div className={checkFocused ? `${s.name} ${s.focused}` : `${s.name}`}>{props.placeholder}</div>
+
+            {checkInput &&
+                <Error meta={meta}/>
+            }
+
         </div>
     );
 };
+
+const Error = ({meta}) => {
+    return (
+        <div className={s.errorWrapper}>
+            <div className={s.error}>
+                <div>{meta.error}</div>
+            </div>
+            <div className={s.icon}>
+                X
+            </div>
+        </div>
+    )
+}
 
 export default Input;
