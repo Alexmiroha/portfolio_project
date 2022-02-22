250469e3e8356213d65eea2ac7eb6fc30ea3c8cc
Alexmiroha <Alexmiroha@mail.ru>
2022-02-22 23:45:59 +0100

    40 Show error, when user type wrong password or email(using stopSubmit dispatch from redux-form L79

--- src/Redux/Reducer-Header.js
+++ src/Redux/Reducer-Header.js
@@ -1,4 +1,5 @@
 import {headerAPI} from "../API/API";
+import {stopSubmit} from "redux-form";
 
 const SET_USER_AUTH = 'SET_USER_AUTH'
 const DELETE_USER_AUTH = 'DELETE_USER_AUTH'
@@ -61,6 +62,10 @@ export const login = (email, password, rememberMe) => (dispatch) => {
         .then(response => {
             if (response.data.resultCode === 0) {
                 dispatch(getProfileHeader())
+            }
+            else {
+                let error = response.data.messages.length > 0 ? response.data.messages[0] : "Unknown error"
+                dispatch(stopSubmit("login", {_error: error}))
             }
         });
 }
--- src/components/MainPageComponents/Login/Login.js
+++ src/components/MainPageComponents/Login/Login.js
@@ -14,25 +14,26 @@ const requiredPassword = requiredCreator('password');
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit} className={s.form}>
-                <div className={s.inputs}>
-                    <Field component={Input} name={"email"} type={"text"} placeholder={"Login"} placeholder={"Login or email"} validate={[requiredLogin, maxLength25]}/>
-                    <Field component={Input} name={"password"} type={"password"} placeholder={"Password"} validate={[requiredPassword, maxLength25]}/>
-                </div>
-                <div className={s.options}>
-                    <label>
-                        <Field component={"input"} name={"rememberMe"} type={"checkbox"} />
-                        Remember Me
-                    </label>
-                    <div className={s.forgot}>Forgot my Password</div>
-                </div>
-
-                <button className={s.button}>Login</button>
-
+            <div className={s.inputs}>
+                <Field component={Input} name={"email"} type={"text"} placeholder={"Login"}
+                       placeholder={"Login or email"} validate={[requiredLogin, maxLength25]}/>
+                <Field component={Input} name={"password"} type={"password"} placeholder={"Password"}
+                       validate={[requiredPassword, maxLength25]}/>
+            </div>
+            {props.error && <div>{props.error}</div>}
+            <div className={s.options}>
+                <label>
+                    <Field component={"input"} name={"rememberMe"} type={"checkbox"}/>
+                    Remember Me
+                </label>
+                <div className={s.forgot}>Forgot my Password</div>
+            </div>
+            <button className={s.button}>Login</button>
         </form>
     )
 };
 
-const LoginReduxForm = reduxForm({form: "login"}) (LoginForm);
+const LoginReduxForm = reduxForm({form: "login"})(LoginForm);
 
 const Login = (props) => {
 
@@ -42,12 +43,11 @@ const Login = (props) => {
 
     if (props.isLogined) {
         return <Redirect to={"/profile"}/>
-    }
-
-    else return (
+    } else return (
         <div className={s.loginComponent}>
             <h1 className={s.title}>Login</h1>
             <LoginReduxForm onSubmit={onSubmit}/>
+
         </div>
     )
 };
@@ -55,9 +55,8 @@ const Login = (props) => {
 // редакс форм устарела, сейчас актуальны редакс файнл форм, формик, реакт хук форм
 
 
-
 const mapStateToProps = (state) => ({
     isLogined: state.header.isLogined
 })
 
-export default connect(mapStateToProps, {login, logout}) (Login);
+export default connect(mapStateToProps, {login, logout})(Login);
