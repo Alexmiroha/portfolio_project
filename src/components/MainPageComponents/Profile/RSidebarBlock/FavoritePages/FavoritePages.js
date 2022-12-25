dfb8666a5f0eb662c62d9a550ba0c0b2a807b421
AlexPC <pssxopatik673@gmail.com>
2022-12-25 18:34:21 +0100

    was added pages to FavPagesRSidebar

--- src/components/MainPageComponents/Profile/RSidebarBlock/FavoritePages/FavoritePages.js
+++ src/components/MainPageComponents/Profile/RSidebarBlock/FavoritePages/FavoritePages.js
@@ -6,24 +6,31 @@ import img2 from './img/avatar42-sm.webp'
 import img3 from './img/avatar43-sm.webp'
 import img4 from './img/avatar44-sm.webp'
 import img5 from './img/avatar45-sm.webp'
+import {TiStarOutline} from "react-icons/ti";
 
+const FavPageBlock = (props) => {
+    return (
+        <div className={s.favPageBlock}>
+            <div className={s.imgBlock}>
+                <img className={s.img} src={img1} alt=""/>
+            </div>
+            <div className={s.titleBlock}>
+                <p className={s.title}>The Marina Bar</p>
+                <p className={s.titleDiscription}>Restaurant / Bar</p>
+            </div>
+            <div className={s.icon}>
+                <TiStarOutline/>
+            </div>
+        </div>
+    )
+}
 
 const FavoritePages = () => {
     return (
         <div className="ProfileSidebar">
             <h6>Favorite Pages</h6>
-            <div className={s.favPageBlock}>
-                <div className={s.img}>
-                    <img src={img1} alt=""/>
-                </div>
-                <div className={s.titleBlock}>
-                    <p className={s.title}></p>
-                    <p className={s.title}></p>
-                </div>
-                <div className={s.icon}>
-{/*тут треба іконку з сайдбара*/}
-                </div>
-            </div>
+            <FavPageBlock/>
+            <FavPageBlock/>
         </div>
     );
 };
