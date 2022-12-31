99517962b07bfc2baba04c6e6d475482a65ad3a4
AlexPC <pssxopatik673@gmail.com>
2022-12-31 20:47:03 +0100

    was added counts of Badges in ProfileBadges

--- src/components/MainPageComponents/Profile/LSidebarBlock/ProfileBadges/ProfileBadges.js
+++ src/components/MainPageComponents/Profile/LSidebarBlock/ProfileBadges/ProfileBadges.js
@@ -15,6 +15,7 @@ const Badge = (props) => {
     return (
         <div className={s.badge}>
             <img src={props.image} alt=""/>
+            {props.count? <div className={s.badgeCount}>{props.count}</div> : <div></div>}
         </div>
     )
 
@@ -25,13 +26,13 @@ const ProfileBadges = () => {
         <div className='ProfileSidebar'>
             <h6 className={s.mainTitle}>Profile Badges</h6>
             <div className={s.badgesBlock}>
-                <Badge image={badge1}/>
+                <Badge image={badge1} count={1}/>
                 <Badge image={badge2}/>
                 <Badge image={badge3}/>
-                <Badge image={badge4}/>
+                <Badge image={badge4} count={2}/>
                 <Badge image={badge5}/>
                 <Badge image={badge6}/>
-                <Badge image={badge7}/>
+                <Badge image={badge7} count={5}/>
                 <Badge image={badge8}/>
                 <Badge image={badge9}/>
                 <Badge image={badge10}/>
