8ea63263100f95b4877e89a53ae3c507df0b509a
AlexLaptop <pssxopatik673@gmail.com>
2023-01-03 13:00:38 +0100

    was added Iframe to LastVideos component

--- src/components/MainPageComponents/Profile/LSidebarBlock/LastVideos/LastVideos.js
+++ src/components/MainPageComponents/Profile/LSidebarBlock/LastVideos/LastVideos.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import s from './LastVideos.module.css'
+import Iframe from 'react-iframe'
 
 const LastVideos = (props) => {
     return (
         <div className='ProfileSidebar'>
             <h6>Last Videos</h6>
+            <div className={s.vidsContainer}>
+                <div className={s.vid}>
+                    <Iframe width="260px" height="auto" src="https://www.youtube.com/embed/Z8Z51no1TD0"
+                            title="YouTube video player" frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullScreen"
+                            >
+                    </Iframe>
+                </div>
+            </div>
         </div>
     );
 };
